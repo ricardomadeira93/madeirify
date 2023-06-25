b7436d66d0d986f15f334ef384c3a16df3b83795
Ricardo Madeira <ricardomadeira1993@gmail.com>
2023-06-25 01:43:41 +0100

    Header Navigation/Signin/up buttons. Content Head

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,9 @@
 import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import Button from "./Button";
 interface HeaderProps {
   children: React.ReactNode;
   className?: string;
@@ -31,7 +34,30 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
             <RxCaretRight className="text-white" size={25} />
           </button>
         </div>
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button className="rounded-full p-2 bg-white items-center justify-center hover:opacity-75 transition">
+            <HiHome />
+          </button>
+          <button className="rounded-full p-2 bg-white items-center justify-center hover:opacity-75 transition">
+            <BiSearch />
+          </button>
+        </div>
+        <div className="flex justify-between items-center gap-x-4">
+          <>
+            <div className="">
+              <Button className="bg-transparent text-neutral font-medium">
+                Sign Up
+              </Button>
+            </div>
+            <div className="">
+              <Button onClick={() => {}} className="bg-white px-6 py-2">
+                Log In
+              </Button>
+            </div>
+          </>
+        </div>
       </div>
+      {children}
     </div>
   );
 };
