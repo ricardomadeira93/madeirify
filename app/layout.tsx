a3e5daab6c2a773119539f4e0b795cc67d120f8e
Ricardo Madeira <ricardomadeira1993@gmail.com>
2023-06-26 18:52:31 +0100

    useUser hook, Providers and going into modal V IMP

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import Sidebar from "@/components/Sidebar";
 import "./globals.css";
 import { Work_Sans } from "next/font/google";
 import SupabaseProvider from "@/providers/supabaseProvider";
+import UserProvider from "@/providers/UserProvider";
+import ModalProvider from "@/providers/ModalProvider";
 
 const font = Work_Sans({ subsets: ["latin"] });
 
@@ -19,7 +21,10 @@ export default function RootLayout({
     <html lang="en" data-theme="forest">
       <body className={font.className}>
         <SupabaseProvider>
-          <Sidebar>{children}</Sidebar>
+          <UserProvider>
+            <ModalProvider />
+            <Sidebar>{children}</Sidebar>
+          </UserProvider>
         </SupabaseProvider>
       </body>
     </html>
