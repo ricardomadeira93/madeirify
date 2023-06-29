4ecb7627716d40a143c574b121ef9c6e5082e9c3
Ricardo Madeira <ricardomadeira1993@gmail.com>
2023-06-29 18:23:35 +0100

    Upload Modal

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,10 +1,24 @@
 "use client";
 import { TbPlaylist } from "react-icons/tb";
 import { AiOutlinePlus } from "react-icons/ai";
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+import useUploadModal from "@/hooks/useUploadModal";
+
 interface LibraryProps {}
 
 const Library: React.FC<LibraryProps> = ({}) => {
-  const onClick = () => {};
+  const authModal = useAuthModal();
+  const uploadModal = useUploadModal();
+  const { user } = useUser();
+  const onClick = () => {
+    if (!user) {
+      return authModal.onOpen();
+    }
+    //subscription
+    return uploadModal.onOpen();
+  };
+
   return (
     <div className="flex flex-col">
       <div className="flex items-center justify-between px-5 pt-5">
@@ -18,7 +32,9 @@ const Library: React.FC<LibraryProps> = ({}) => {
           size={22}
         />
       </div>
-      <div className="flex flex-col gap-y-2 mt-4 px-3 text-white">Song List</div>
+      <div className="flex flex-col gap-y-2 mt-4 px-3 text-white">
+        Song List
+      </div>
     </div>
   );
 };
